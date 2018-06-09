776f0142c4e18608e90d6437053d428be9d40453
Shumei Huang <rose1264@gmail.com>
2018-06-09 12:23:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each do |attendee|
-
+    room_assignments.push("Hello, #{attendee}! You'll be assigned to room 1!")
   end
   room_assignments
 end
