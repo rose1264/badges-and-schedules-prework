25a45f082094a0907730336520683bcca7397704
Shumei Huang <rose1264@gmail.com>
2018-06-09 12:28:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  puts batch_badge_creator(attendees[0])
+  puts assign_rooms(attendees[0])
 end
